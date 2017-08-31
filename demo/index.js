94530c0327e5124711c20e9993aa31b69172e164
Jeroen van Veen <j.veenvan@gmail.com>
2017-08-31 14:31:46 +0200

    Cleanup.

--- demo/index.js
+++ demo/index.js
@@ -1,25 +1,16 @@
-const {Shout, Shouts, vuex} = require('../src/js/index.js')
+const {Shout, Shouts} = require('../src/js/index.js')
 
 Vue.component('Shout', Shout)
 Vue.component('Shouts', Shouts)
 
-const vuexStore = new Vuex.Store({
-    actions: vuex.actions,
-    mutations: vuex.mutations,
-    state: vuex.state,
-})
-
 new Vue({
     el: '#app',
-    store: vuexStore,
     data: {
         version: require('../package.json').version,
     },
     methods: {
         addShout: function() {
-            vuexStore.dispatch('notify', {
-                message: 'Shout it all out!',
-            })
+
         },
     },
 })

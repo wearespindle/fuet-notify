eed4355569d46109b81cb911f3d4be93520f80ee
Jeroen van Veen <j.veenvan@gmail.com>
2018-01-22 17:49:16 +0100

    Add icon.

--- src/js/notification.js
+++ src/js/notification.js
@@ -1,12 +1,22 @@
 module.exports = function(template) {
     return {
-        render: template.r,
-        staticRenderFns: template.s,
         props: ['notification'],
+        render: template.r,
         methods: {
-            closeNotification: function(notification) {
+            iconCss: function(notification) {
+                let cssClasses = {}
+                cssClasses[`fa-${notification.icon}`] = true
+                return cssClasses
+            },
+            notificationCss: function(notification) {
+                let cssClasses = {}
+                if (notification.type) cssClasses[`is-${notification.type}`] = true
+                return cssClasses
+            },
+            close: function(notification) {
                 this.$store.notifications = this.$store.notifications.filter((i) => i.id !== notification.id)
             },
         },
+        staticRenderFns: template.s,
     }
 }
--- src/vue/notification.vue
+++ src/vue/notification.vue
@@ -1,9 +1,14 @@
 <transition name="fade">
-<div class="notification is-primary">
-    <button class="delete" @click="closeNotification(notification)"></button>
-    <div v-if="html" v-html="message"></div>
-    <div v-else>
+<div class="notification" :class="notificationCss(notification)">
+    <div class="icon">
+        <i class="fa" :class="iconCss(notification)" v-if="notification.icon"></i>
+    </div>
+    <div class="message" v-if="html" v-html="message"></div>
+    <div class="message" v-else>
         {{ notification.message }}
+    </div>
+    <div class="actions">
+        <button class="delete" @click="close(notification)"></button>
     </div>
 </div>
 </transition>
